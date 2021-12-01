b1e0b06ff41b5159e8c7b1c51255d8932fa4e172
YazawaKenichi <tonfarken83825@gmail.com>
2021-12-02 00:03:29 +0900

    delta

---
+++ Yazawa_Codes/（未完成）マウスホバーで四角形を表示するテスト.py
@@ -0,0 +1,22 @@
+"""
+	Special Thanks!
+	https://qiita.com/East_san/items/8f4938967a605cecd2ef
+"""
+
+"""		import		"""
+
+import cv2
+
+"""		priparation		"""
+
+filepass = input("pass")
+source_image = cv2.imread(filepass)
+
+"""		define		"""
+
+def mouse_callback(event, x, y, flags, userdata):
+
+"""			main		"""
+
+
+
